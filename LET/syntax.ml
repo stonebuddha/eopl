d02ec6b92a7027c01d11d0026a4cef87b63cc1f8
Di Wang <wayne.wangdi@yandex.com>
2016-10-02 00:46:04 -0400

    start to work on LET, a simple langauge

---
+++ LET/syntax.ml
@@ -0,0 +1,31 @@
+type program =
+  | AProgram of expression
+
+and expression =
+  | ConstExp of int * Ploc.t
+  | DiffExp of expression * expression * Ploc.t
+  | IsZeroExp of expression * Ploc.t
+  | IfExp of expression * expression * expression * Ploc.t
+  | VarExp of string * Ploc.t
+  | LetExp of string * expression * expression * Ploc.t
+
+let g = Grammar.gcreate (Plexer.gmake ())
+
+let p = Grammar.Entry.create g "program"
+let e = Grammar.Entry.create g "expression"
+
+let parse = Grammar.Entry.parse p
+
+EXTEND
+  p : [
+    [ exp1 = e -> AProgram exp1 ]
+  ];
+  e : [
+    [ num = INT -> ConstExp (int_of_string num, loc)
+    | "-"; "("; exp1 = e; ","; exp2 = e; ")" -> DiffExp (exp1, exp2, loc)
+    | "is_zero"; "("; exp1 = e; ")" -> IsZeroExp (exp1, loc)
+    | "if"; exp1 = e; "then"; exp2 = e; "else"; exp3 = e -> IfExp (exp1, exp2, exp3, loc)
+    | var = LIDENT -> VarExp (var, loc)
+    | "let"; var = LIDENT; "="; exp1 = e; "in"; body = e -> LetExp (var, exp1, body, loc) ]
+  ];
+END
---
+++ LET/syntax.mli
@@ -0,0 +1,12 @@
+type program =
+  | AProgram of expression
+
+and expression =
+  | ConstExp of int * Ploc.t
+  | DiffExp of expression * expression * Ploc.t
+  | IsZeroExp of expression * Ploc.t
+  | IfExp of expression * expression * expression * Ploc.t
+  | VarExp of string * Ploc.t
+  | LetExp of string * expression * expression * Ploc.t
+
+val parse : char Stream.t -> program
