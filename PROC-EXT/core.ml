182fe9ac9acc71b892f6f295fd9a406e0ecb4577
Di Wang <wayne.wangdi@yandex.com>
2016-10-05 22:17:25 -0400

    add exercise 3.20 for PROC

---
+++ PROC-EXT/core.ml
@@ -0,0 +1,65 @@
+open Syntax
+
+type environment = (string * expval) list
+
+and expval =
+  | NumVal of int
+  | BoolVal of bool
+  | ProcVal of proc
+
+and proc = string * expression * environment
+
+let string_of_expval eval =
+  match eval with
+  | NumVal num -> string_of_int num
+  | BoolVal bool -> string_of_bool bool
+  | ProcVal _ -> "<proc>"
+
+let empty_env () = []
+
+let extend_env var eval env = (var, eval) :: env
+
+let apply_env env var = List.assoc var env
+
+exception Interpreter_error of string * Ploc.t
+
+let rec value_of exp env =
+  match exp with
+  | ConstExp (num, loc) -> NumVal num
+  | DiffExp (exp1, exp2, loc) ->
+    let eval1 = value_of exp1 env in
+    let eval2 = value_of exp2 env in
+    (match (eval1, eval2) with
+     | (NumVal num1, NumVal num2) -> NumVal (num1 - num2)
+     | _ -> raise (Interpreter_error ("the operands of diff should be numbers", loc)))
+  | IsZeroExp (exp1, loc) ->
+    let eval1 = value_of exp1 env in
+    (match eval1 with
+     | NumVal num1 -> BoolVal (num1 = 0)
+     | _ -> raise (Interpreter_error ("the operand of is_zero should be a number", loc)))
+  | IfExp (exp1, exp2, exp3, loc) ->
+    let eval1 = value_of exp1 env in
+    (match eval1 with
+     | BoolVal bool1 -> if bool1 then value_of exp2 env else value_of exp3 env
+     | _ -> raise (Interpreter_error ("the predicate of if should be a boolean", loc)))
+  | VarExp (var, loc) ->
+    (try apply_env env var
+     with Not_found -> raise (Interpreter_error ("the variable " ^ var ^ " is not in the environment", loc)))
+  | LetExp (var, exp1, body, loc) ->
+    let eval1 = value_of exp1 env in
+    value_of body (extend_env var eval1 env)
+  | ProcExp (var, body, loc) -> ProcVal (var, body, env)
+  | CallExp (rator, rand, loc) ->
+    let rator_val = value_of rator env in
+    (match rator_val with
+     | ProcVal proc -> let rand_val = value_of rand env in apply_procedure proc rand_val
+     | _ -> raise (Interpreter_error ("the operator of call shoud be a procedure", loc)))
+
+and apply_procedure proc arg_val =
+  match proc with
+  | (var, body, saved_env) -> value_of body (extend_env var arg_val saved_env)
+
+let value_of_top_level (ExpTop exp1) =
+  value_of exp1 (empty_env ()) |> string_of_expval |> print_endline
+
+let value_of_program (AProgram tops) = List.iter value_of_top_level tops
