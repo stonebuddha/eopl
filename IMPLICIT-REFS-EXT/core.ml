4cd7a94bfeae697b905ee3064af79d5928593786
Di Wang <wayne.wangdi@yandex.com>
2016-10-30 17:32:28 -0400

    add exercise 4.18, 4.19 and a constant-time store for IMPLICIT-REFS

---
+++ IMPLICIT-REFS-EXT/core.ml
@@ -0,0 +1,128 @@
+open Syntax
+
+type refer = int
+
+and store = expval list
+
+and environment = refer list
+
+and expval =
+  | NumVal of int
+  | BoolVal of bool
+  | ProcVal of proc
+
+and proc = expression * environment ref
+
+let string_of_expval eval =
+  match eval with
+  | NumVal num -> string_of_int num
+  | BoolVal bool -> string_of_bool bool
+  | ProcVal _ -> "<proc>"
+
+let empty_env () = []
+
+let extend_env refer env = refer :: env
+
+let apply_env env var = List.nth env var
+
+let default_store_size = 1024 * 1024
+
+let empty_store () = (ref 0, ref (Array.make 0 (NumVal 0)))
+
+let init_store (next, st) =
+  let () = next := 0 in
+  let () = st := Array.make default_store_size (NumVal 0) in
+  ()
+
+let newref eval (next, st) =
+  if (!next) < default_store_size then
+    let refer = !next in
+    let () = next := refer + 1 in
+    let () = (!st).(refer) <- eval in
+    refer
+  else
+    raise Not_found
+
+let deref refer (_, st) = (!st).(refer)
+
+let setref refer eval (_, st) = (!st).(refer) <- eval
+
+let the_store = empty_store ()
+
+exception Interpreter_error of string * Ploc.t
+
+let rec value_of exp env =
+  match exp with
+  | ConstExp (num, loc) -> NumVal num
+  | DiffExp (exp1, exp2, loc) ->
+    let eval1 = value_of exp1 env in
+    let eval2 = value_of exp2 env in
+    (match (eval1, eval2) with
+     | (NumVal num1, NumVal num2) -> NumVal (num1 - num2)
+     | _ -> raise (Interpreter_error ("the operands of diff should be numbers", loc)))
+  | IsZeroExp (exp1, loc) ->
+    let eval1 = value_of exp1 env in
+    (match eval1 with
+     | NumVal num1 -> BoolVal (num1 = 0)
+     | _ -> raise (Interpreter_error ("the operand of is_zero should be a number", loc)))
+  | IfExp (exp1, exp2, exp3, loc) ->
+    let eval1 = value_of exp1 env in
+    (match eval1 with
+     | BoolVal bool1 -> if bool1 then value_of exp2 env else value_of exp3 env
+     | _ -> raise (Interpreter_error ("the predicate of if should be a boolean", loc)))
+  | VarExp (var, loc) -> deref (apply_env env var) the_store
+  | LetExp (exps, body, loc) ->
+    let evals = List.map (fun exp1 -> value_of exp1 env) exps in
+    let env' =
+      try List.fold_left (fun env eval1 -> extend_env (newref eval1 the_store) env) env evals
+      with Not_found -> raise (Interpreter_error ("there is no space in the store", loc)) in
+    value_of body env'
+  | ProcExp (body, loc) -> ProcVal (body, ref env)
+  | CallExp (rator, rand, loc) ->
+    let rator_val = value_of rator env in
+    (match rator_val with
+     | ProcVal proc -> let rand_val = value_of rand env in apply_procedure proc rand_val loc
+     | _ -> raise (Interpreter_error ("the operator of call should be a procedure", loc)))
+  | LetrecExp (p_bodies, letrec_body, loc) ->
+    let saved_env = ref (empty_env ()) in
+    let procs = List.map (fun p_body -> ProcVal (p_body, saved_env)) p_bodies in
+    let env' =
+      try List.fold_left (fun env eval -> extend_env (newref eval the_store) env) env procs
+      with Not_found -> raise (Interpreter_error ("there is no space in the store", loc)) in
+    let () = saved_env := env' in
+    value_of letrec_body !saved_env
+  | BeginExp (exps, loc) ->
+    let eval1 = value_of (List.hd exps) env in
+    List.fold_left (fun _ exp1 -> value_of exp1 env) eval1 (List.tl exps)
+  | AssignExp (var, exp1, loc) ->
+    let refer = apply_env env var in
+    let eval1 = value_of exp1 env in
+    let () = setref refer eval1 the_store in
+    NumVal 27
+
+and apply_procedure proc arg_val loc =
+  match proc with
+  | (body, saved_env) ->
+    let env =
+      try extend_env (newref arg_val the_store) !saved_env
+      with Not_found -> raise (Interpreter_error ("there is no space in the store", loc)) in
+    value_of body env
+
+let value_of_top_level top env =
+  match top with
+  | ValTop (var, exp1, loc) ->
+    let eval1 = value_of exp1 env in
+    let () = print_endline ("val " ^ var ^ " = " ^ string_of_expval eval1 ^ ";") in
+    (try extend_env (newref eval1 the_store) env
+     with Not_found -> raise (Interpreter_error ("there is no space in the store", loc)))
+  | FunTop (p_name, p_body, loc) ->
+    let saved_env = ref (empty_env ()) in
+    let proc = ProcVal (p_body, saved_env) in
+    let () =
+      try saved_env := extend_env (newref proc the_store) env
+      with Not_found -> raise (Interpreter_error ("there is no space in the store", loc)) in
+    !saved_env
+
+let value_of_program (AProgram tops) =
+  let () = init_store the_store in
+  List.fold_left (fun env top -> value_of_top_level top env) (empty_env ()) tops; ()
---
+++ IMPLICIT-REFS-EXT/syntax.ml
@@ -0,0 +1,86 @@
+type program =
+  | AProgram of top_level list
+
+and top_level =
+  | ValTop of string * expression * Ploc.t
+  | FunTop of string * expression * Ploc.t
+
+and expression =
+  | ConstExp of int * Ploc.t
+  | DiffExp of expression * expression * Ploc.t
+  | IsZeroExp of expression * Ploc.t
+  | IfExp of expression * expression * expression * Ploc.t
+  | VarExp of int * Ploc.t
+  | LetExp of expression list * expression * Ploc.t
+  | ProcExp of expression * Ploc.t
+  | CallExp of expression * expression * Ploc.t
+  | LetrecExp of expression list * expression * Ploc.t
+  | BeginExp of expression list * Ploc.t
+  | AssignExp of int * expression * Ploc.t
+
+let empty_ctx () = []
+
+let extend_ctx var ctx = var :: ctx
+
+let rec apply_ctx var ctx =
+  match ctx with
+  | [] -> raise Not_found
+  | saved_var :: saved_ctx -> if var = saved_var then 0 else 1 + apply_ctx var saved_ctx
+
+exception Parser_error of string * Ploc.t
+
+let g = Grammar.gcreate (Plexer.gmake ())
+
+let p = Grammar.Entry.create g "program"
+let t = Grammar.Entry.create g "top level"
+let e = Grammar.Entry.create g "expression"
+let l = Grammar.Entry.create g "let binding"
+let r = Grammar.Entry.create g "letrec binding"
+
+let parse = Grammar.Entry.parse p
+
+EXTEND
+  p : [
+    [ tops = LIST0 t ->
+      let (tops, ctx) = List.fold_left (
+          fun (tops, ctx) top ->
+            let (top, ctx) = top ctx in
+            (top :: tops, ctx)) ([], empty_ctx ()) tops in
+      AProgram (List.rev tops) ]
+  ];
+  t : [
+    [ exp1 = e; ";" -> fun ctx -> (ValTop ("it", exp1 ctx, loc), extend_ctx "it" ctx)
+    | "val"; var = LIDENT; "="; exp1 = e; ";" -> fun ctx -> (ValTop (var, exp1 ctx, loc), extend_ctx var ctx)
+    | "fun"; p_name = LIDENT; "(";  b_var = LIDENT; ")"; "="; p_body = e; ";" -> fun ctx -> (FunTop (p_name, p_body (extend_ctx b_var (extend_ctx p_name ctx)), loc), extend_ctx p_name ctx) ]
+  ];
+  e : [
+    [ num = INT -> fun ctx -> ConstExp (int_of_string num, loc)
+    | "-"; "("; exp1 = e; ","; exp2 = e; ")" -> fun ctx -> DiffExp (exp1 ctx, exp2 ctx, loc)
+    | "is_zero"; "("; exp1 = e; ")" -> fun ctx -> IsZeroExp (exp1 ctx, loc)
+    | "if"; exp1 = e; "then"; exp2 = e; "else"; exp3 = e -> fun ctx -> IfExp (exp1 ctx, exp2 ctx, exp3 ctx, loc)
+    | var = LIDENT -> fun ctx ->
+        (try VarExp (apply_ctx var ctx, loc)
+         with Not_found -> raise (Parser_error ("the variable " ^ var ^ " is unbound", loc)))
+    | "let"; binds = LIST0 l; "in"; body = e -> fun ctx ->
+        let (vars, exps) = List.split binds in
+        let ctx' = List.fold_left (fun ctx var -> extend_ctx var ctx) ctx vars in
+        LetExp (List.map (fun exp -> exp ctx) exps, body ctx', loc)
+    | "proc"; "("; var = LIDENT; ")"; body = e -> fun ctx -> ProcExp (body (extend_ctx var ctx), loc)
+    | "("; rator = e; rand = e; ")" -> fun ctx -> CallExp (rator ctx, rand ctx, loc)
+    | "letrec"; binds = LIST0 r; "in"; letrec_body = e -> fun ctx ->
+        let (p_names, binds) = List.split binds in
+        let ctx' = List.fold_left (fun ctx var -> extend_ctx var ctx) ctx p_names in
+        let p_bodies = List.map (fun (b_var, p_body) -> p_body (extend_ctx b_var ctx')) binds in
+        LetrecExp (p_bodies, letrec_body ctx', loc)
+    | "begin"; exps = LIST1 e SEP ";"; "end" -> fun ctx -> BeginExp (List.map (fun exp -> exp ctx) exps, loc)
+    | "set"; var = LIDENT; "="; exp1 = e -> fun ctx ->
+        (try AssignExp (apply_ctx var ctx, exp1 ctx, loc)
+         with Not_found -> raise (Parser_error ("the variable " ^ var ^ " is unbound", loc))) ]
+  ];
+  l : [
+    [ var = LIDENT; "="; exp1 = e -> (var, exp1) ]
+  ];
+  r : [
+    [ p_name = LIDENT; "("; b_var = LIDENT; ")"; "="; p_body = e -> (p_name, (b_var, p_body)) ]
+  ];
+END
